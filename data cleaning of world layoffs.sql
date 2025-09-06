4f76e9a1b7f274ec68fbc6ea2359e3faa1e08ddd
avinashpatil2140 <avinashpatil2140@gmail.com>
2025-09-06 18:10:10 +0530

    Add files via upload

---
+++ data cleaning of world layoffs.sql
@@ -0,0 +1,162 @@
+select *
+from layoffs;
+
+
+create table layoffs_staging
+like layoffs;
+
+insert layoffs_staging
+select *
+from layoffs;
+
+select *
+from layoffs_staging;
+
+with duplicate_cte as
+(
+select *, 
+row_number() over( partition by company, industry, total_laid_off, percentage_laid_off, `date`,stage, country, funds_raised_millions) as row_num
+from layoffs_staging
+)
+select*
+from duplicate_cte
+where row_num >1;
+
+select * 
+from layoffs_staging
+where company = 'casper';
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select * 
+from layoffs_staging2;
+
+insert into layoffs_staging2
+select *, 
+row_number() over( partition by company, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num
+from layoffs_staging;
+
+select * 
+from layoffs_staging2;
+
+
+
+
+
+
+
+select company, trim(company)
+from layoffs_staging2;
+
+update layoffs_staging2
+set company = trim(company);
+
+select distinct industry
+from layoffs_staging2
+order by 1;
+
+select distinct *
+from layoffs_staging2
+where industry like 'crypto%';
+
+update layoffs_staging2
+set industry='crypto'
+where industry like 'crypto%';
+
+select distinct location
+from layoffs_staging2
+order by 1;
+
+select distinct country , trim(trailing '.' from country)
+from layoffs_staging2
+order by 1;
+
+update layoffs_staging2
+set country = trim(trailing  '.' from country)
+where country like 'United States%';
+
+
+select `date`,
+str_to_date(`date`, '%m/%d/%Y')
+from layoffs_staging2;
+
+update layoffs_staging2
+set `date` = str_to_date(`date`, '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column `date` date;
+
+select *
+from layoffs_staging2;
+
+
+
+
+
+
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select  *
+from layoffs_staging2
+where company = 'Airbnb';
+
+update layoffs_staging2
+set industry = null
+where industry = '';
+
+select *
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company=t1.company
+where (t1.industry is null or t1.industry ='')
+and t2.industry is not null;
+
+
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company=t1.company
+    set t1.industry = t2.industry
+where t1.industry is null 
+and t2.industry is not null;
+
+select industry 
+from layoffs_staging2
+where industry is null;
+
+
+
+
+
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+delete 
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+select *
+from layoffs_staging2;
+
+
+alter table layoffs_staging2
+drop column row_num;
